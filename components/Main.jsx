63ddbdc4a0b8217b7ce33bbc2765e4235c453f87
Gefferson Casasola <gcasasolah@uni.pe>
2023-03-12 20:35:13 -0500

    Add changes to main component

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,43 +4,41 @@ import { TypeAnimation } from 'react-type-animation';
 
 export const Main = () => {
     return (
-        <div className='w-full h-screen text-center'>
+        <div id='main' className='w-full h-screen text-center'>
             
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 
                 <div>
-                    <p className='uppercase text-sm tracking-widest text-[#5651e5]'>
+                    <p className='uppercase text-sm tracking-widest text-[#FFFFFF]'>
                         LET&#39;S BUILD SOMETHING TOGETHER
                     </p>
-                    <h1 className='py-4 text-[#5651e5]'>
-                        Hi, I&#39;m <span className='text-[#2174FF]'> Gefferson</span>
+                    <h1 className='py-4 text-[#FFFFFF]'>
+                        Hi, I&#39;m <span className='text-[#FFFFFF]'> Gefferson</span>
 
                     </h1>
                     
                     <TypeAnimation
                         sequence={[
-                            'a Backend Developer', 
+                            "a Backend Developer", 
                             2000, 
-                            'a Electronic Engineer', 
+                            "a Electronic Engineer", 
                             2000, 
                         ]}
                         wrapper="h2"
                         cursor={true}
                         repeat={Infinity}
                         style={{ fontSize: '3em' }}
-                        className='py-2 text-[#00A3FF]'
+                        className='py-2 text-[#FFFFFF]'
                     />
-                    <p className='py-4 text-[#1654BD] sm:max-w-[70%] m-auto'>
-                        I develop scalable and functional web applications using Python with Django, JavaScript and TypeScript with Node.js for the backend and React and Bootstrap for the frontend.
-                    </p>
-                    <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                    <br />
+                    <div className='flex items-center justify-between max-w-[330px] m-auto py-8'>
                         <a
                             href='https://www.linkedin.com/in/gefferson-casasola/'
                             target='_blank'
                             rel='noreferrer'
                         >
-                            <div className='rounded-full shadow-lg shadow-green-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <FaLinkedinIn />
+                            <div className='rounded-full shadow-lg p-2 cursor-pointer hover:scale-110 ease-in duration-300 bg-cyan-100'>
+                                <FaLinkedinIn className='text-[#006BFF]' size='30'/>
                             </div>
                         </a>
                         <a
@@ -48,8 +46,8 @@ export const Main = () => {
                             target='_blank'
                             rel='noreferrer'
                         >
-                            <div className='rounded-full shadow-lg shadow-green-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <FaGithub />
+                            <div className='rounded-full shadow-lg p-2 cursor-pointer hover:scale-110 ease-in duration-300 bg-cyan-100'>
+                                <FaGithub className='text-[#006BFF]' size='30'/>
                             </div>
                         </a>
                         <a
@@ -57,8 +55,8 @@ export const Main = () => {
                             target='_blank'
                             rel='noreferrer'
                         >
-                            <div className='rounded-full shadow-lg shadow-green-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <FaTwitter />
+                            <div className='rounded-full shadow-lg p-2 cursor-pointer hover:scale-110 ease-in duration-300 bg-cyan-100'>
+                                <FaTwitter className='text-[#006BFF]' size='30'/>
                             </div>
                         </a>
                         <a
@@ -66,8 +64,8 @@ export const Main = () => {
                             target='_blank'
                             rel='noreferrer'
                         >
-                            <div className='rounded-full shadow-lg shadow-green-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <FaFacebook />
+                            <div className='rounded-full shadow-lg p-2 cursor-pointer hover:scale-110 ease-in duration-300 bg-cyan-100'>
+                                <FaFacebook className='text-[#006BFF]' size='30'/>
                             </div>
                         </a>
                     </div>
