151ae269162d9d4f79b54868fb91d55ad6d56812
Gefferson Casasola <gcasasolah@uni.pe>
2023-03-12 19:40:15 -0500

    Add new socials media in Main component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
-import { AiOutlineMail } from 'react-icons/ai';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
-import Link from 'next/link';
+import { FaGithub, FaLinkedinIn, FaTwitter, FaFacebook } from 'react-icons/fa';
+import { TypeAnimation } from 'react-type-animation';
 
 export const Main = () => {
     return (
         <div className='w-full h-screen text-center'>
+            
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+                
                 <div>
-                    <p className='uppercase text-sm tracking-widest text-gray-600'>
+                    <p className='uppercase text-sm tracking-widest text-[#5651e5]'>
                         LET&#39;S BUILD SOMETHING TOGETHER
                     </p>
-                    <h1 className='py-4 text-gray-700'>
-                        Hi, I&#39;m <span className='text-[#5651e5]'> Gefferson</span>
+                    <h1 className='py-4 text-[#5651e5]'>
+                        Hi, I&#39;m <span className='text-[#2174FF]'> Gefferson</span>
 
                     </h1>
-                    <h1 className='py-2 text-gray-700'>A Backend Web Developer</h1>
-                    <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
+                    
+                    <TypeAnimation
+                        sequence={[
+                            'a Backend Developer', 
+                            2000, 
+                            'a Electronic Engineer', 
+                            2000, 
+                        ]}
+                        wrapper="h2"
+                        cursor={true}
+                        repeat={Infinity}
+                        style={{ fontSize: '3em' }}
+                        className='py-2 text-[#00A3FF]'
+                    />
+                    <p className='py-4 text-[#1654BD] sm:max-w-[70%] m-auto'>
                         I develop scalable and functional web applications using Python with Django, JavaScript and TypeScript with Node.js for the backend and React and Bootstrap for the frontend.
                     </p>
                     <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
@@ -26,7 +39,7 @@ export const Main = () => {
                             target='_blank'
                             rel='noreferrer'
                         >
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <div className='rounded-full shadow-lg shadow-green-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                                 <FaLinkedinIn />
                             </div>
                         </a>
@@ -35,20 +48,28 @@ export const Main = () => {
                             target='_blank'
                             rel='noreferrer'
                         >
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <div className='rounded-full shadow-lg shadow-green-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                                 <FaGithub />
                             </div>
                         </a>
-                        <Link href='/#contact'>
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <AiOutlineMail />
+                        <a
+                            href="https://twitter.com/gcasasolah"
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <div className='rounded-full shadow-lg shadow-green-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                <FaTwitter />
                             </div>
-                        </Link>
-                        <Link href='/resume'>
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                <BsFillPersonLinesFill />
+                        </a>
+                        <a
+                            href="https://www.facebook.com/gefferson.casasola"
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <div className='rounded-full shadow-lg shadow-green-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                <FaFacebook />
                             </div>
-                        </Link>
+                        </a>
                     </div>
                 </div>
             </div>
