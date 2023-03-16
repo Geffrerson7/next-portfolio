2feaeb677cfaf1cc0a37131cfdedddb8b0a4c41d
Gefferson Casasola <gcasasolah@uni.pe>
2023-03-15 22:21:44 -0500

    Add fix in about component

--- components/About.jsx
+++ components/About.jsx
@@ -13,8 +13,15 @@ const About = () => {
                     </p>
                     <h2 className='py-4'>Who I Am?</h2>
                     <p className='py-2 text-gray-600'>
-                        Hello! I'm Gefferson. I'm a junior programmer who loves to build things with code.
-                        My first experience with programming was when I was 22 years old at university while I was studying electronic engineering, but it didn't last long: I was a big fan of video games, and one day I saw in a university lecture how to create a pacman video game with C++ and decided to do it. At first it was difficult for me because I did not understand the logic and syntax but after watching tutorials and asking friends for advice I was able to do it. Then I learned Matlab and I liked the graphs I made for the Linear and Discrete Control Systems course. On March 1, 2023, I graduated from the 18-week Beca Bicentenario Scholarship Intensive Bootcamp 3.0: Backend Developer and am looking for a full-time position where I can help a company achieve their
+                        Hello! I&#39;m Gefferson. I&#39;m a junior programmer who loves to build things with code.
+                        My first experience with programming was when I was 22 years old at university while I was 
+                        studying electronic engineering, but it didn&#39;t last long: I was a big fan of video games, 
+                        and one day I saw in a university lecture how to create a pacman video game with C++ and decided 
+                        to do it. At first it was difficult for me because I did not understand the logic and syntax but 
+                        after watching tutorials and asking friends for advice I was able to do it. Then I learned Matlab 
+                        and I liked the graphs I made for the Linear and Discrete Control Systems course. On March 1, 2023, 
+                        I graduated from the 18-week Beca Bicentenario Scholarship Intensive Bootcamp 3.0: Backend Developer 
+                        and am looking for a full-time position where I can help a company achieve their
                         goals.
                     </p>
 
